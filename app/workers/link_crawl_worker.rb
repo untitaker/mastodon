21d2a515091372696dbe57aa2a7184f17d70a9a3
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-01-16 21:16:27 +0100

    tag status domain of linkcrawler

--- app/workers/link_crawl_worker.rb
+++ app/workers/link_crawl_worker.rb
@@ -6,7 +6,9 @@ class LinkCrawlWorker
   sidekiq_options queue: 'pull', retry: 0
 
   def perform(status_id)
-    FetchLinkCardService.new.call(Status.find(status_id))
+    status = Status.find(status_id)
+    Sentry.set_tags('mastodon.status.domain': status.account.domain)
+    FetchLinkCardService.new.call(status)
   rescue ActiveRecord::RecordNotFound
     true
   end
